f806d1e17b894912acba6ceca1d03590d4576e1a
vonbarown <vonielbrown@pursuit.org>
2019-11-26 20:37:40 -0500

    select box implementation

--- forms/src/components/userForm.jsx
+++ forms/src/components/userForm.jsx
@@ -4,18 +4,32 @@ class UserInfoForm extends React.Component {
     constructor() {
         super();
         this.initialState = {
-            usernameValue: "someone",
-            userPassword: "passcode",
-            submitted: false
+            usernameValue: "",
+            userPassword: "",
+            submitted: false,
+            notARobot: false,
+            title: ''
         };
         this.state = this.initialState;
     }
 
+    allFieldsValid = () => {
+        return (
+            this.state.notARobot &&
+            this.state.title &&
+            this.state.usernameValue &&
+            this.state.userPassword
+        );
+    };
     handleSubmitted = event => {
         event.preventDefault();
-        this.setState({
-            submitted: true
-        });
+        if (this.allFieldsValid()) {
+            console.log("help", this.state);
+
+            this.setState({
+                submitted: true
+            });
+        }
     };
 
     userChange = event => {
@@ -32,15 +46,30 @@ class UserInfoForm extends React.Component {
         });
     };
 
+
     resetForm = event => {
         event.preventDefault();
         this.setState(this.initialState);
     };
 
+    handleCheckBox = (event) => {
+        // console.log(this.state);
+        console.log(('Checked', event.target.checked));
+        this.setState({
+            notARobot: event.target.checked
+        })
+    }
+
+    handleSelectChange = (event) => {
+        this.setState({
+            title: event.target.value
+        })
+    }
+
     render() {
         console.log(this.state);
 
-        const { usernameValue, userPassword, submitted } = this.state;
+        const { usernameValue, userPassword, submitted, notARobot, title } = this.state;
         if (submitted) {
             return (
                 <div>
@@ -51,6 +80,16 @@ class UserInfoForm extends React.Component {
             return (
                 <form onSubmit={this.handleSubmitted}>
                     <h2>User Information</h2>
+                    <label htmlFor="title">Title:
+                        <select id="title" onChange={this.handleSelectChange} value={title}>
+                            <option value=''></option>
+                            <option value='mr'>Mr.</option>
+                            <option value='ms'>Ms.</option>
+                            <option value='mrs'>Mrs.</option>
+                            <option value='mx'>Mx.</option>
+                            <option value='dr'>Dr.</option>
+                        </select>
+                    </label>
                     <input
                         type="text"
                         placeholder="username"
@@ -63,6 +102,14 @@ class UserInfoForm extends React.Component {
                         value={userPassword}
                         onChange={this.userPassChange}
                     />
+                    <label htmlFor="not-robot">not a Robot
+                        <input
+                            id="not-robot"
+                            type="checkbox"
+                            onChange={this.handleCheckBox}
+                            checked={notARobot}
+                        />
+                    </label>
                     <button>Submit</button>
                     <button onClick={this.resetForm}>Reset</button>
                 </form>
